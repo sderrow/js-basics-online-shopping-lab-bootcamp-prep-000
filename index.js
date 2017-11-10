a57380c7c5ad4e53e168198401d484542498cad2
Sean Derrow <sderrow@mit.edu>
2017-11-10 19:32:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,5 +65,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  } else {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
